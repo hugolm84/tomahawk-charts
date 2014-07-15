3cf5580d0fbbe1917840f4a19882ee1daad49cba
Hugo Lindström <hugolm84@gmail.com>
2014-07-15 13:30:10 +0200

    Add geo to rdio

--- scraper/tomahawk/spiders/rdiospider.py
+++ scraper/tomahawk/spiders/rdiospider.py
@@ -30,14 +30,17 @@ class RdioSpider(TomahawkCrawlSpider):
     oauth_consumer_secret = 'yt35kakDyW'
 
     # Regions, might change http://www.rdio.com/availability/
-    regions = [ "US", "SE", "CA", "DE", "GB", "AU",
-                "BE", "BR", "DK", "EE", "FI", "FR",
-                "IS", "IE","IT", "LV", "LT", "NL",
-                "NZ", "NO", "PT", "ES"]
+    regions = [ "US"]#, "SE", "CA", "DE", "GB", "AU",
+                #"BE", "BR", "DK", "EE", "FI", "FR",
+                #"IS", "IE","IT", "LV", "LT", "NL",
+                #"NZ", "NO", "PT", "ES"]
     default_region = "US"
     default_type = "Track"
 
     base_types = ["Artist", "Album", "Track"]
+
+    def __init__(self, name=None, **kwargs):
+        super(RdioSpider, self).__init__()
 
     def start_requests(self):
         for base_type in self.base_types:
@@ -54,6 +57,7 @@ class RdioSpider(TomahawkCrawlSpider):
         chart.add_value("name", name)
         chart.add_value("id", name+type+region)
         chart.add_value("type", type)
+        chart.add_value("geo", region)
         chart.add_value("description", "%s %s's in %s" % (name, type, region))
         return chart
 
