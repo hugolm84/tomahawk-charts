aa1339986e3abb3f9a31b70efa61f487e8a07758
Hugo Lindström <hugolm84@gmail.com>
2016-02-06 12:37:55 +0100

    Fix Billboard. Deny scraping of realtime charts.

--- scraper/tomahawk/spiders/billboardspider.py
+++ scraper/tomahawk/spiders/billboardspider.py
@@ -29,12 +29,16 @@ class BillboardSpider(TomahawkCrawlSpider):
         'http://www.billboard.com/charts',
     ]
 
+    # Realtime charts, Get the JSON at: http://realtime.billboard.com/tracks/?chart=1&chartName=Overall&limit=20&offset=0
+    # Note: We do not include these in the charts
+
     chart_items_xpath = '//span[@class="field-content"]'
     next_page_xpath = '//li[@class="pager-next last"]'
 
     rules = (
         TomahawkCrawlSpider.follow_link_as_chart(
             xpath=chart_items_xpath,
+            deny=['realtime'],
         ),
         TomahawkCrawlSpider.follow_link_as_next(
             xpath=next_page_xpath,
@@ -48,7 +52,7 @@ class BillboardSpider(TomahawkCrawlSpider):
     track_charts = ['hot 100', 'airplay', 'youtube', 'adult contemporary', 'ringtones']
 
     def do_create_chart(self, chart, response):
-        name = extract('//h1[@id="page-title"]/text()', chart.selector)
+        name = extract('//span[@class="how-it-works__title"]/text()', chart.selector)
         chart.add_value("name", name)
         chart.add_value("type", self.extract_type(chart))
         chart.add_value("description", 'Description')
@@ -56,27 +60,23 @@ class BillboardSpider(TomahawkCrawlSpider):
 
     def do_parse(self, chart, response):
         selector = Selector(response)
-        items_xpath = '//div[@class="listing chart_listing"]/article/header'
+        items_xpath = '//div[contains(@class,"chart-data")]/div[@class="container"]/article[contains(@class,"chart-row")]'
         items = selector.xpath(items_xpath)
 
         item_type = self.do_get_type(chart)
 
         for item in items:
             entry = TomahawkItemLoader(selector=item)
-            entry.add_xpath("rank", './/span/text()')
-            entry.add_xpath(item_type, './/h1/text()')
+            entry.add_xpath("rank", './/div[@class="chart-row__primary"]/div[@class="chart-row__rank"]/span/text()')
 
-            # Almost all items have a <a> field for artists.
-            # First try to extract the text() from the a field, if that doesnt work, try to extract text() from the
-            # selector
+            # H2 is Artist or Song
+            entry.add_xpath(item_type, './/div[@class="chart-row__primary"]/div[@class="chart-row__title"]/h2/text()')
+            entry.add_xpath(item_type, './/div[@class="track"]/span[@class="track-info"]/span[@class="web-only"]/a/text()')
 
-            artist_selector = item.xpath('.//p[@class="chart_info"]')
-            artist = extract('.//a/text()', artist_selector)
+            if item_type is not 'Artist':
+                entry.add_xpath("artist", './/div[@class="chart-row__primary"]/div[@class="chart-row__title"]/h3/text()')
+                entry.add_xpath("artist", './/div[@class="chart-row__primary"]/div[@class="chart-row__title"]/h3/a/text()')
 
-            if not artist:
-                artist = extract('.//text()', artist_selector)
-
-            entry.add_value("artist", artist)
             chart.add_value("list", entry.load_item())
 
         # process the item if there is no more next_pages, otherwise, return none and keep parsing
